70bef46ebab9a816d11ca7c3b1329a8053864cf9
Renatas Daunys <renatas.daunys@gmail.com>
2017-10-01 00:05:55 +0100

    .

---
+++ Design-Paterns/Main/Structural Patterns/Facade Patern/EnglishMetricConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.Structural_Patterns.Facade_Patern
+{
+    public class EnglishMetricConverter
+    {
+        public double FarenheitToCelcious(double degrees)
+        {
+            return ((degrees - 32) / 9.0) * 5.0;
+        }
+    }
+}
---
+++ Design-Paterns/Main/Structural Patterns/Facade Patern/TemperatureFacade.cs
@@ -0,0 +1,39 @@
+﻿using Main.Structural_Patterns.Facade_Patern.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.Structural_Patterns.Facade_Patern
+{
+    public class TemperatureFacade
+    {
+        readonly WeatherService weatherService;
+        readonly EnglishMetricConverter converter;
+
+        public TemperatureFacade()
+            : this(new WeatherService(), new EnglishMetricConverter())
+        { }
+
+        public TemperatureFacade(WeatherService weatherService, EnglishMetricConverter englishMetricConverter)
+        {
+            this.weatherService = weatherService;
+            this.converter = englishMetricConverter;
+        }
+
+        public LocalTemperature GetTemperature()
+        {
+            var farenheit = weatherService.GetTempFarenheit();
+            var celcius = converter.FarenheitToCelcious(farenheit);
+
+            var localTemperature = new LocalTemperature()
+            {
+                Farenheit = farenheit,
+                Celcius = celcius,
+            };
+
+            return localTemperature;
+        }
+    }
+}
