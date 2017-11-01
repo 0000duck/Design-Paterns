c35e94e1ebb4c16d5e5ba783f5c93364d54dfaad
Renatas Daunys <renatas.daunys@gmail.com>
2017-11-01 22:18:21 +0000

    .

---
+++ UnitTests/AbstractFactoryPatternTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{   
+    [TestClass]
+    public class AbstractFactoryPatternTest
+    {
+        [TestMethod]
+        public void TestShowImageShouldBeTrue()
+        {
+            //IImage imageProxy = new HighResImageProxy("path");
+            //Assert.IsTrue(imageProxy.ShowImage());
+        }
+    }
+}
