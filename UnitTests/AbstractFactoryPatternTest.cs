e1495ccd9b7e9b4e32c921ea33ee8d53808633eb
Renatas Daunys <renatas.daunys@gmail.com>
2017-11-21 09:13:21 +0000

    Added tests for abstract factory

--- UnitTests/AbstractFactoryPatternTest.cs
+++ UnitTests/AbstractFactoryPatternTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Main.Creational_Patterns.Abstract_Factory_Pattern;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +12,25 @@ namespace UnitTests
     public class AbstractFactoryPatternTest
     {
         [TestMethod]
-        public void TestShowImageShouldBeTrue()
+        public void ShouldBuildSportBike()
         {
-            //IImage imageProxy = new HighResImageProxy("path");
-            //Assert.IsTrue(imageProxy.ShowImage());
+            IVehicleFactory factory = new HondaFactory();
+            var client = new VehicleClient(factory, VehicleType.Sports);
+
+            var name = client.GetBikeName();
+
+            Assert.AreEqual("Sports Bike", name);
+        }
+
+        [TestMethod]
+        public void ShouldBuildRegularBike()
+        {
+            IVehicleFactory factory = new HondaFactory();
+            var client = new VehicleClient(factory, VehicleType.Regular);
+
+            var name = client.GetBikeName();
+
+            Assert.AreEqual("Regular Bike", name);
         }
     }
 }
