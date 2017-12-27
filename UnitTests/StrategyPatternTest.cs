e8c48b6ab36674921fb520158d749965432d83e7
daunysre <daunysre@GBLJ5VVXF2>
2017-12-27 20:12:45 +0000

    Added strategy pattern

---
+++ UnitTests/StrategyPatternTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Main.Behavioral_Patterns.Strategy_Pattern;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    class StrategyPatternTest
+    {
+        [TestMethod]
+        public void ShouldReturnCostForOneDayDelivery()
+        {
+            var calculator = new DeliveryCalculator(new OneDayDeliveryCalculator());
+            var totalCost = calculator.CalculateShippingCost(10);
+            Assert.AreEqual(25, totalCost);
+        }
+
+        [TestMethod]
+        public void ShouldReturnCostForStandartDelivery()
+        {
+            var calculator = new DeliveryCalculator(new StandartDeliveryCalculator());
+            var totalCost = calculator.CalculateShippingCost(10);
+            Assert.AreEqual(15, totalCost);
+        }
+    }
+}
