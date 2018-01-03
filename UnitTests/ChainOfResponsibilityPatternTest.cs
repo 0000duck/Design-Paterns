567d7bd485a74d6d9bff69dc788a44ff2dbc967a
Renatas Daunys <renatas.daunys@gmail.com>
2018-01-03 22:22:08 +0000

    added test for chain of responsibility

---
+++ UnitTests/ChainOfResponsibilityPatternTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ChainOfResponsibilityPatternTest
+    {
+        Approver clerk;
+        Approver assistantManager;
+        Approver manager;
+        public ChainOfResponsibilityPatternTest()
+        {
+            // Setup Chain of Responsibility
+            clerk = new Clerk();
+            assistantManager = new AssistantManager();
+            manager = new Manager();
+        }
+
+        [TestMethod]
+        public void Test()
+        {            
+            
+        }
+    }
+
+    internal class Manager : Approver
+    {
+    }
+
+    internal class Approver
+    {
+    }
+
+    internal class AssistantManager : Approver
+    {
+    }
+
+    internal class Clerk : Approver
+    {
+    }
+}
