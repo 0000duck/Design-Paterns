8f8d43588ec7af2cef69f42e3e128691c307e57b
Renatas Daunys <renatas.daunys@gmail.com>
2018-01-15 20:38:35 +0000

    added observer pattern

---
+++ Design-Paterns/Main/Behavioral Patterns/Observer Pattern/IBM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.Behavioral_Patterns.Observer_Pattern
+{
+    public class IBM : Stock
+    {
+        public IBM(string symbol, double price) : base(symbol, price) { }
+    }
+}
---
+++ Design-Paterns/Main/Behavioral Patterns/Observer Pattern/Investor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.Behavioral_Patterns.Observer_Pattern
+{
+    public class Investor : IInvestor
+    {
+        private string _name;
+        private Stock _stock;
+        public Investor(string name)
+        {
+            this._name = name;
+        }
+        public void Update(Stock stock)
+        {
+            Stock = stock;
+        }
+        public Stock Stock
+        {
+            get { return _stock; }
+            set { _stock = value; }
+        }
+    }
+}
